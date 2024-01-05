2c0fed033af97196799a45f405c2e5926caab0ed
hmarsho14 <75851181+hmarsho14@users.noreply.github.com>
2024-01-04 19:45:33 -0500

    progress check 2

--- PAU_exercise2_code.R
+++ PAU_exercise2_code.R
@@ -7,6 +7,7 @@
 # install.packages("lubridate") <- run, if necessary
 library(dplyr)
 library(lubridate)
+library(stats)
 
 setwd("~/Documents/GitHub/PAU")
 
@@ -50,6 +51,56 @@ write.csv(refined_race_ethn, "~/Documents/GitHub/PAU/refined_race_eth.csv", row.
 
 # From the aggregate data, in which month did the PPD conduct the most vehicle stops? 
 
-refined_month <- refined %>% group_by(month = floor_date(datetimeoccur, "month")) %>% summarise(count = n()) %>% # create table
+refined_month <- refined %>% group_by(month = floor_date(datetimeoccur, "month")) %>% summarise(count = n()) %>% replace(1, c('January', 'February', 'March')) # create table
 
-write.csv(refined_month, "~/Documents/GitHub/PAU/refined_race_eth.csv", row.names = FALSE) # export table
+write.csv(refined_month, "~/Documents/GitHub/PAU/refined_month.csv", row.names = FALSE) # export table
+
+#=========================================================================
+# Question 4
+#=========================================================================
+
+# From the aggregate data, on which day of the week were vehicles most likely to be stopped?
+
+refined_weekday <- refined %>% group_by(weekday) %>% summarise(count = n()) # create table
+
+write.csv(refined_weekday, "~/Documents/GitHub/PAU/refined_weekday.csv", row.names = FALSE) # export table
+
+#=========================================================================
+# Question 5
+#=========================================================================
+
+# Were more motorists stopped in District 12 or District 24? 
+
+refined_district <- refined %>% group_by(districtoccur) %>% summarise(count = n()) # create table
+
+write.csv(refined_district, "~/Documents/GitHub/PAU/refined_district.csv", row.names = FALSE) # export table
+
+#=========================================================================
+# Question 6
+#=========================================================================
+
+# What is the breakdown of vehicle stops by race/ethnicity in District 12 compared to District 24?
+
+refined_race_district <- table(refined$race, refined$districtoccur) # create two-way table
+
+write.csv(refined_race_district, "~/Documents/GitHub/PAU/refined_race_district.csv", row.names = TRUE) # export table
+
+#=========================================================================
+# Question 7
+#=========================================================================
+
+# Calculate the frisk rate in District 12 for each racial/ethnic identity.
+
+refined_frisked <- refined %>% filter(districtoccur == 12) %>% group_by(race) %>% summarise(frisk_rate = round(sum(vehicle_frisked) / n() * 100, 2)) # create table
+
+write.csv(refined_frisked, "~/Documents/GitHub/PAU/refined_frisked.csv", row.names = TRUE) # export table
+
+#=========================================================================
+# Question 8
+#=========================================================================
+
+# Are there any observable differences in vehicle stops in District 12 versus District 24? 
+# What might account for such differences, and how would you go about testing your hypotheses?
+
+chisq.test()
+
